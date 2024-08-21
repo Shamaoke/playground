93397ee6120f0b36cee357cea665279353007b9b
Shamaoke <shamaoke@yandex.ru>
2024-08-21 15:24:16 +0300

    [rust] Edit questions_answers

--- rust/questions_answers/src/main.rs
+++ rust/questions_answers/src/main.rs
@@ -1,13 +1,33 @@
 
 ///
-/// ::: Questions/Answers :::
+///                                      █████    ███
+///                                     ▒▒███    ▒▒▒
+///    █████████████ ████  ██████  ████████████  ████   ██████  ████████   █████
+///   ███▒▒███▒▒███ ▒███  ███▒▒██████▒▒▒▒▒███▒  ▒▒███  ███▒▒███▒▒███▒▒███ ███▒▒
+///  ▒███ ▒███ ▒███ ▒███ ▒███████▒▒█████ ▒███    ▒███ ▒███ ▒███ ▒███ ▒███▒▒█████
+///  ▒███ ▒███ ▒███ ▒███ ▒███▒▒▒  ▒▒▒▒███▒███ ███▒███ ▒███ ▒███ ▒███ ▒███ ▒▒▒▒███
+///  ▒▒███████ ▒▒████████▒▒██████ ██████ ▒▒█████ █████▒▒██████  ████ ███████████
+///   ▒▒▒▒▒███  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒ ▒▒▒▒▒▒   ▒▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒  ▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒
+///       ▒███   ██████  ████████   █████ █████ ███ █████ ██████  ████████  █████
+///       █████    ▒▒███▒▒███▒▒███ ███▒▒ ▒▒███ ▒███▒▒███ ███▒▒███▒▒███▒▒██████▒▒
+///      ▒▒▒▒▒   ███████ ▒███ ▒███▒▒█████ ▒███ ▒███ ▒███▒███████  ▒███ ▒▒▒▒▒█████
+///             ███▒▒███ ▒███ ▒███ ▒▒▒▒███▒▒███████████ ▒███▒▒▒   ▒███     ▒▒▒▒███
+///            ▒▒████████████ ███████████  ▒▒████▒████  ▒▒██████  █████    ██████
+///             ▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒ ▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒    ▒▒▒▒▒▒
 ///
-
-use std::fmt::{Display, Formatter};
 
 use std::str::FromStr;
 use std::io::{Error, ErrorKind};
 
+use warp::{Filter, Reply, Rejection};
+use warp::reply::json;
+use warp::reject::custom;
+use warp::reject::Reject;
+use warp::http::StatusCode;
+
+use serde::Serialize;
+
+#[derive(Serialize)]
 struct Question {
   id: QuestionId,
   title: String,
@@ -15,8 +35,15 @@ struct Question {
   tags: Option<Tags>,
 }
 
+#[derive(Serialize)]
 struct QuestionId(String);
 
+#[derive(Debug)]
+struct InvalidId;
+
+impl Reject for InvalidId { }
+
+#[derive(Serialize)]
 struct Tags {
   list: Vec<String>,
 }
@@ -24,24 +51,6 @@ struct Tags {
 impl Question {
   fn new(id: QuestionId, title: String, content: String, tags: Option<Tags>) -> Self {
     Question { id, title, content, tags }
-  }
-}
-
-impl Display for Question {
-  fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-    write!(f, "{0}) {1}\n{2}\n[{3}]", self.id, self.title, self.content, self.tags.as_ref().unwrap_or(&Tags { list: Vec::from([String::from("none")]) }).list.join(", "))
-  }
-}
-
-impl Display for QuestionId {
-  fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-    write!(f, "{}", self.0)
-  }
-}
-
-impl Display for Tags {
-  fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-    write!(f, "{}", self.list.join(" "))
   }
 }
 
@@ -56,20 +65,52 @@ impl FromStr for QuestionId {
   }
 }
 
-fn main( ) -> ( ) {
+async fn get_questions( ) -> Result<impl Reply, Rejection> {
 
-  let tags = Tags { list: Vec::from([String::from("rust"), String::from("string")]) };
-
-  let question_1 = Question::new(
+  let question = Question::new(
     QuestionId::from_str("1").expect("No ID provided"),
-    String::from("What is the equivalent of the join operator over a vector of Strings?"),
-    String::from(r#"I wasn't able to find the Rust equivalent for the "join" operator over a vector of Strings."#),
-    Some(tags),
+    String::from("Frist question"),
+    String::from("First question content"),
+    Some(Tags { list: vec!(String::from("faq")) }),
   );
 
-  println!("{}", question_1);
-  // println!("{}", question_1.tags.unwrap().list.join(", "));
+  match question.id.0.parse::<i32>() {
+    Ok(_)  => Ok(json(&question)),
+    Err(_) => Err(custom(InvalidId))
+  }
+}
 
-  return ( );
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+
+  if let Some(_invalid_id) = r.find::<InvalidId>() {
+    Ok(warp::reply::with_status(
+      "No valid ID presented",
+      StatusCode::UNPROCESSABLE_ENTITY,
+    ))
+  } else {
+    Ok(warp::reply::with_status(
+       "Route not found",
+       StatusCode::NOT_FOUND,
+    ))
+  }
+}
+
+#[tokio::main]
+async fn main( ) {
+
+  let get_items = warp::get()
+    .and(warp::path("questions"))
+    .and(warp::path::end())
+    .and_then(get_questions)
+    .recover(return_error);
+
+  warp::serve(get_items).run(([127, 0, 0, 1], 1337)).await;
+
+  // Example requests
+  // ----------------
+  // `curl --silent http://127.0.0.1:1337/questions`
+  // `curl --silent http://127.0.0.1:1337/questions | jq .title`
+  // `curl --silent http://127.0.0.1:1337/questions | jq '.tags.list | .[0]'
+  //
 }
 
