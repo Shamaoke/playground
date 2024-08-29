546237f5f6b323fb00302c150b6fdcc1ad4915c4
Shamaoke <shamaoke@yandex.ru>
2024-08-29 08:28:42 +0300

    [rust] Edit scenario1

--- rust/scenario1/tcpclient/src/main.rs
+++ rust/scenario1/tcpclient/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+
+///
+/// ::: TCP Client :::
+///
+
+use std::env::{ args };
+use std::str::FromStr;
+use std::net::{ TcpStream, SocketAddr };
+
+fn main( ) {
+
+  let port = u16::from_str(&args().nth(1).unwrap()).unwrap();
+
+  // println!("{}", u16::from_str(&args().nth(1).unwrap()).unwrap())
+
+  // for arg in args() { println!("{}"); }
+
+  let socket = SocketAddr::from(([127, 0, 0, 1], port));
+
+  // let _stream = TcpStream::connect(socket).unwrap();
+
+  if let Ok(_stream) = TcpStream::connect(socket) {
+    println!("Connected to the server!");
+  } else {
+    println!("Couldn't connect to server...");
+  }
 }
+
--- rust/scenario1/tcpserver/src/main.rs
+++ rust/scenario1/tcpserver/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+
+///
+/// ::: TCP Server :::
+///
+
+use std::net::{ TcpListener, TcpStream, SocketAddr };
+use std::io::{ Error };
+
+fn handle_connection(stream: Result<TcpStream, Error>) {
+  let _stream = stream.unwrap();
+  println!("Connection established");
 }
+
+fn main( ) {
+
+  // let socket = SocketAddr::from(([127, 0, 0, 1], 4433));
+
+  let socket = SocketAddr::from(([127, 0, 0, 1], 0));
+
+  let listener = TcpListener::bind(socket).unwrap();
+
+  println!("Running on port {}", listener.local_addr().unwrap().port());
+
+  // for stream in listener.incoming() { handle_connection(stream); }
+
+  listener.incoming().for_each(handle_connection);
+}
+
