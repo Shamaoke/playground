94b3368eccac2282e6d0b73cae859adda36ad22a
Shamaoke <shamaoke@yandex.ru>
2024-09-28 15:36:31 +0300

    [rails] Showtime | Edit movies_controller

--- rails/showtime/app/controllers/movies_controller.rb
+++ rails/showtime/app/controllers/movies_controller.rb
@@ -5,11 +5,13 @@ class MoviesController < ApplicationController
 
   def new = ( @movie, @genres = [ Movie.new, Genre.all ] )
 
-  def create = Movie.new(movie_params) { _1.save and redirect_to movies_path }
+  def create =
+    Movie.new(movie_params) { _1.save and redirect_to movies_path }
 
   private
-    def movie_params = params
-                         .require(:movie)
-                         .permit(:title, :release_year, { genre_ids: [ ] })
+    def movie_params =
+      params
+        .require(:movie)
+        .permit(:title, :release_year, { genre_ids: [ ] })
 end
 
