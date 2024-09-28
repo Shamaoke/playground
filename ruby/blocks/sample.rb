523879b3077641fb0901c71de2758f7b44836292
Shamaoke <shamaoke@yandex.ru>
2024-09-28 15:37:15 +0300

    [ruby] Create blocks/sample.rb

---
+++ ruby/blocks/sample.rb
@@ -0,0 +1,38 @@
+
+=begin
+
+END { do_work }
+
+def do_work = (
+  puts 'ok!'
+  puts 'ok too!'
+  puts 'ok free!'
+)
+
+=end
+
+=begin
+END { p index_a, index_b }
+
+def index_a = nil
+def index_b = ( )
+=end
+
+=begin
+END { index }
+
+    def index = (
+      1 + 1
+      2 + 2
+      3 + 3
+    )
+=end
+
+=begin
+END { index }
+
+def do_one(val) = val
+
+def index = do_one('ok!').then { puts _1 }
+=end
+
