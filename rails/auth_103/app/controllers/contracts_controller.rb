bae176838462dda99c5b136c53b5535f7c6c1680
Shamaoke <shamaoke@yandex.ru>
2024-10-16 08:15:31 +0300

    [rails] Auth 103 | Perform small changes to the application

--- rails/auth_103/app/controllers/contracts_controller.rb
+++ rails/auth_103/app/controllers/contracts_controller.rb
@@ -1,6 +1,8 @@
 
 class ContractsController < ApplicationController
-  def index = ( @contracts = Contract.all )
+  def index
+    @contracts = Contract.all
+  end
 
   def show
     @contract = Contract.find_by(id: params[:id])
