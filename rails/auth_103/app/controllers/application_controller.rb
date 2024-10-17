0c3e85f56c64234d3e6937da7060baa946d8d47b
Shamaoke <shamaoke@yandex.ru>
2024-10-17 10:52:00 +0300

    [rails] Autho 103 | Develop the system views

--- rails/auth_103/app/controllers/application_controller.rb
+++ rails/auth_103/app/controllers/application_controller.rb
@@ -6,9 +6,10 @@ class ApplicationController < ActionController::Base
   private
     def current_user
       Rails.logger.info('CURRENT USER IS CALLED')
-      cookies[:user_id] \
-        and Current.user = User.find_by_id(cookies[:user_id]) \
-        or Current.user = User.new
+      # cookies[:user_id] \
+      #   and Current.user = User.find_by_id(cookies[:user_id]) \
+      #   or Current.user = User.new
+      Current.user = (cookies[:user_id] and User.find_by_id(cookies[:user_id]) or User.new)
     end
 
     def user_policy
