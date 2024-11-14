660301b4d84e13c1163861a0c89c8e35100021e7
Shamaoke <shamaoke@yandex.ru>
2024-11-14 21:38:44 +0300

    [rails] Auth 103 | Develop the application

--- rails/auth_103/app/controllers/application_controller.rb
+++ rails/auth_103/app/controllers/application_controller.rb
@@ -8,11 +8,19 @@ class ApplicationController < ActionController::Base
       # cookies[:user_id] \
       #   and Current.user = User.find_by_id(cookies[:user_id]) \
       #   or Current.user = User.new
-      Current.user = (cookies[:user_id] and User.find_by_id(cookies[:user_id]) or User.new)
+      # Current.user = (cookies[:user_id] and User.find_by_id(cookies[:user_id]) or User.new)
+
+      Current.user = begin
+        cookies[:user_id] and User.find_by_id(cookies[:user_id]) or User.new
+      end
     end
 
     def user_policy
       Current.user
     end
+
+    # def not_found!
+    #   render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+    # end
 end
 
--- rails/auth_103/app/controllers/users_controller.rb
+++ rails/auth_103/app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:id])
+    # @user = User.find_by_id(params[:id]) or not_found!
+    @user = User.find_by_id!(params[:id])
     begin
+      # authorize! @user if @user
       authorize! @user
     rescue ActionPolicy::Unauthorized
       redirect_to users_path, alert: t('messages.unauthorized')
