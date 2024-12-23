94fc6f90b5b50e9673699313c243a72c8bebbf84
Shamaoke <shamaoke@yandex.ru>
2024-12-23 07:00:34 +0300

    [cpp] Create Import STD

---
+++ cpp/import_std/main.cpp
@@ -0,0 +1,12 @@
+
+#include <string>
+
+void hello_world(std::string const& name);
+
+auto main( ) -> int {
+
+  hello_world("Pavel");
+
+  return 0;
+}
+
---
+++ cpp/import_std/uses_std.cpp
@@ -0,0 +1,10 @@
+
+import std;
+
+auto hello_world(std::string const& name) -> void {
+
+  std::cout << "Hello, world!" << "\n" << "My name is" << " " << name << "." << "\n";
+
+  return void();
+}
+
