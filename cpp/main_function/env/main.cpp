56a0f525be8ec0551263a2f4cc1cce7e10f40c6c
Shamaoke <shamaoke@yandex.ru>
2024-12-27 06:10:52 +0300

    [cpp] MainFunction | Put template files to Env

---
+++ cpp/main_function/env/main.cpp
@@ -0,0 +1,13 @@
+
+///
+/// ::: MainFunction | Env :::
+///
+
+// #include <algorithm>
+// #include <format>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+auto main(int argc, char** argv, char** env) -> int { return 0; }
+
