5953d58b05c596a6df7c28b84eaf673f6b13bab2
Shamaoke <shamaoke@yandex.ru>
2025-01-15 15:05:03 +0300

    [rspec] Bacon | Edit spec

--- rspec/bacon/spec/bacon_spec.rb
+++ rspec/bacon/spec/bacon_spec.rb
@@ -1,8 +1,21 @@
 
+require 'bacon'
+
 RSpec.describe Bacon do
+
   it 'is edible' do
-    expect(Bacon.edible?).to be(true)
+    # expect(Bacon.edible?).to be(true)
     # expect(Bacon).to be_edible
+
+    # expect(Bacon.new.edible?).to(true)
+    expect(subject.edible?).to be(true)
+  end
+
+  it 'can expire' do
+
+    subject.expired!
+
+    expect(subject).to_not be_edible
   end
 end
 
