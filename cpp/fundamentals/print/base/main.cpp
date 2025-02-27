b0c40aa02085dbe4b214db2c8de1892928203d8c
Shamaoke <shamaoke@yandex.ru>
2025-02-27 15:24:46 +0300

    [cpp] Fundamentals | Edit Print/Base

--- cpp/fundamentals/print/base/main.cpp
+++ cpp/fundamentals/print/base/main.cpp
@@ -5,13 +5,12 @@
 
 // #include <algorithm>
 #include <cstdio>
-// #include <format>
+#include <format>
 // #include <iostream>
 #include <print>
 // #include <string>
+#include <string_view>
 // #include <vector>
-
-#include "person.hpp"
 
 ///
 /// Details:
@@ -26,11 +25,32 @@
 /// • [std::literals::string_literals::operator""s - cppreference.com](https://en.cppreference.com/w/cpp/string/basic_string/operator""s)
 ///
 
+namespace {
+
+  class Person {
+
+    private:
+      const std::string_view name { };
+      const std::string_view surname { };
+
+    public:
+      Person(const std::string_view name, const std::string_view surname) :
+        name { name },
+        surname { surname }
+      { }
+
+      auto get_full_name( ) const -> std::string {
+        return std::format("{0} {1}", this->name, this->surname);
+      }
+  };
+}
+
 auto main(int argc, char** argv, char** env) -> int {
 
-  using namespace std::string_literals;
+  // using namespace std::string_literals;
+  using namespace std::string_view_literals;
 
-  auto person { Person { "Pavel"s, "Alekhin"s } };
+  const auto person { Person { "Pavel"sv, "Alekhin"sv } };
 
   std::println(stderr, "{0:*^21}", person.get_full_name());
 
