991fc3be35ee99410070e2da2d76b6a61ffcfb21
Shamaoke <shamaoke@yandex.ru>
2025-02-28 15:20:02 +0300

    [cpp] Edit Constructors

--- cpp/constructors/main.cpp
+++ cpp/constructors/main.cpp
@@ -1,29 +1,86 @@
 
-#include <iostream>
+#include <print>
 
-struct Sample {
+// #include <iostream>
 
-  Sample( ) { std::cout << "Default constructor was called!\n"; }
+// struct Sample {
+//
+//   Sample( ) { std::cout << "Default constructor was called!\n"; }
+//
+//   Sample([[maybe_unused]] Sample& s) { std::cout << "Copy constructor was called!\n"; }
+//
+//   Sample& operator = ([[maybe_unused]] const Sample& s) {
+//
+//     std::cout << "Copy assignment operator was applied!\n";
+//
+//     return *this;
+//   }
+// };
 
-  Sample([[maybe_unused]] Sample& s) { std::cout << "Copy constructor was called!\n"; }
+namespace {
 
-  Sample& operator = ([[maybe_unused]] const Sample& s) {
+  class Sample {
 
-    std::cout << "Copy assignment operator was applied!\n";
+    public:
+      auto f( ) && -> int {
 
-    return *this;
-  }
-};
+        std::println("ok!");
+
+        return 0;
+      }
+
+      auto g( ) & -> int {
+
+        std::println("ok too!");
+
+        return 0;
+      }
+  };
+
+}
 
 auto main( ) -> int {
 
-  Sample s { };
+  ///
+  /// Sample s { };
+  /// Sample& r { s };
+  ///
+  /// s.g(); //~> ok too!
+  /// r.g(); //~> ok too!
+  ///
 
-  Sample s2 { s };
+  ///
+  /// Sample { }.g();
+  /// error: 'this' argument to member function 'g' is an rvalue, but function has non-const lvalue ref-qualifier
+  ///    44 |   Sample { }.g();
+  ///       |   ^
+  /// main.cpp:32:12: note: 'g' declared here
+  ///    32 |       auto g( ) & -> int {
+  ///       |            ^
+  ///
 
-  Sample s3 { };
+  /// Sample { }.f(); //~> ok!
 
-  s3 = s;
+  ///
+  /// Sample s { };
+  ///
+  /// s.f();
+  ///
+  /// error: 'this' argument to member function 'f' is an lvalue, but function has rvalue ref-qualifier
+  ///    41 |   s.f();
+  ///       |   ^
+  /// note: 'f' declared here
+  ///    25 |       auto f( ) && -> int {
+  ///       |            ^
+  ///
+
+  // Sample s { };
+
+  // Sample s2 { s };
+
+  // Sample s3 { };
+
+  // s3 = s;
 
   return 0;
 }
