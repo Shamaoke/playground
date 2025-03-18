7bd550313fe2204d8e9b9fd519ec1dac2612a6b8
Shamaoke <shamaoke@yandex.ru>
2025-03-18 11:04:12 +0300

    [rails] Quotes Editor | Develop the application

--- rails/quotes_editor/app/javascript/application.js
+++ rails/quotes_editor/app/javascript/application.js
@@ -1,3 +1,12 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
-import "@hotwired/turbo-rails"
+
+/// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import 'controllers'
+import '@hotwired/turbo-rails'
+
+///
+/// Disable Turbo Drive for the whole application
+/// import { Turbo } from '@hotwired/turbo-rails'
+///
+/// Turbo.session.drive = false
+///
+
