5addcbd73e1b6ccbd7252f028f2a3bee040078d4
Shamaoke <shamaoke@yandex.ru>
2025-03-21 10:07:42 +0300

    [rails] Quotes Editor | Develop the application

--- rails/quotes_editor/app/models/quote.rb
+++ rails/quotes_editor/app/models/quote.rb
@@ -1,5 +1,18 @@
 
 class Quote < ApplicationRecord
+
   validates :name, presence: true
+
+  # after_create_commit -> {
+  #   ##
+  #   ## [Module: Turbo::Broadcastable — Documentation for turbo-rails (2.0.13)](https://rubydoc.info/gems/turbo-rails/Turbo/Broadcastable#broadcast_append_to-instance_method)
+  #   ##
+  #   broadcast_append_to(
+  #     'quotes',
+  #     partial: 'quotes/quote',
+  #     locals: { quote: self },
+  #     target: 'quotes'
+  #   )
+  # }
 end
 
