0203b06302039a1ed8e762029a2770614f4a8e68
Shamaoke <shamaoke@yandex.ru>
2025-03-31 10:03:28 +0300

    [rails] ASUDPP • Contracts 001 | Develop the application

--- rails/asudpp/contracts-001/app/controllers/contracts_controller.rb
+++ rails/asudpp/contracts-001/app/controllers/contracts_controller.rb
@@ -13,13 +13,31 @@ class ContractsController < ApplicationController
     @contract = Contract.new(params.expect(contract: [:name, :date, :content]))
 
     if @contract.save
-      respond_to do |foramt|
+      respond_to do |format|
         format.html do
           redirect_to contracts_path
         end
       end
     else
       render :new
+    end
+  end
+
+  def edit
+    @contract = Contract.find_by(id: params[:id])
+  end
+
+  def update
+    @contract = Contract.find_by(id: params[:id])
+
+    if @contract.update(params.expect(contract: [:name, :date, :content]))
+      respond_to do |format|
+        format.html do
+          redirect_to contracts_path
+        end
+      end
+    else
+      render :edit
     end
   end
 end
