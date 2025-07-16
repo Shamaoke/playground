4b86693224d805aed8e5d3e1d80c758d1fdcb784
Shamaoke <shamaoke@yandex.ru>
2025-07-16 03:01:00 +0300

    [javascript] Davis | Develop Sample 001

--- javascript/davis/sample-001/src/index.js
+++ javascript/davis/sample-001/src/index.js
@@ -5,6 +5,7 @@ let app = express()
 let port = 3355
 
 app.get('/', (req, resp) => {
+  console.log(`[${new Date()}][\x1b\x5b\x33\x34\x6dINFO\x1b\x5b\x30\x6d] Sending greeting to a new visitor...`)
   resp.send('Hello, world!')
 })
 
