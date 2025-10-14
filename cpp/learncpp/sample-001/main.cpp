432f554f2d79d569940a8fd35748822b6b7c2ad2
Shamaoke <shamaoke@yandex.ru>
2025-10-15 02:35:50 +0300

    [cpp] Learn CPP | Develop the project

--- cpp/learncpp/sample-001/main.cpp
+++ cpp/learncpp/sample-001/main.cpp
@@ -1,9 +1,9 @@
 
-#include <format>
-#include <print>
+// #include <format>
+// #include <print>
 
 // #include <unistd.h>
-%:include <unistd.h>
+// %:include <unistd.h>
 /// Details about the `%:` syntax:
 /// [Alternative operator representations - cppreference.com](https://en.cppreference.com/w/cpp/language/operator_alternative.html#Alternative_tokens)
 
@@ -11,7 +11,7 @@
 // #include <vector>
 // #include <array>
 
-// #define SUCCESS 0
+#define SUCCESS 0
 
 // namespace Status {
 //
@@ -21,38 +21,37 @@
 //
 // }
 
-enum class Status {
-  SUCCESS,
-  FAIL,
-};
-
+// enum class Status {
+//   SUCCESS,
+//   FAIL,
+// };
 
 /// The namespace contain functions which color text strings.
-namespace Color {
-
-  /// Color a text string green.
-  auto green(std::string str) -> decltype(str) {
-    return std::format("\x{1b}\x{5b}\x{33}\x{32}\x{6d}{0}\x{1b}\x{5b}\x{30}\x{6d}", str);
-  }
-
-  /// Color a text string yellow.
-  auto yellow(std::string str) -> decltype(str) {
-    return std::format("\x{1b}\x{5b}\x{33}\x{33}\x{6d}{0}\x{1b}\x{5b}\x{30}\x{6d}", str);
-  }
-
-};
+// namespace Color {
+//
+//   /// Color a text string green.
+//   auto green(std::string str) -> decltype(str) {
+//     return std::format("\x{1b}\x{5b}\x{33}\x{32}\x{6d}{0}\x{1b}\x{5b}\x{30}\x{6d}", str);
+//   }
+//
+//   /// Color a text string yellow.
+//   auto yellow(std::string str) -> decltype(str) {
+//     return std::format("\x{1b}\x{5b}\x{33}\x{33}\x{6d}{0}\x{1b}\x{5b}\x{30}\x{6d}", str);
+//   }
+//
+// };
 
 // auto main( ) -> decltype(static_cast<int>(Status::SUCCESS)) {
 auto main( ) -> int {
 
-  using namespace Color; // green(std::string)
+  // using namespace Color; // green(std::string)
 
   // auto str { "Hello, world!" };
   // auto path { std::getenv("PATH") };
 
   /// std::string hostname { };
   /// or
-  char hostname[256] { };
+  // char hostname[256] { };
   /// or
   /// auto hostname { new char[255] }; // then `delete[] hostname` when the object is no longer needed
   /// or
@@ -65,7 +64,7 @@ auto main( ) -> int {
   /// or
   /// std::array<char, 256> hostname { };
 
-  gethostname(hostname, sizeof(hostname));
+  // gethostname(hostname, sizeof(hostname));
 
   // std::println("{0}", green(str));
   // std::println("{0}", path);
@@ -75,24 +74,26 @@ auto main( ) -> int {
 
   // auto build_configuration { "" };
 
-  auto build_configuration <% "" %>;
+  // auto build_configuration <% "" %>;
   /// Details about the `<% %>` syntax:
   /// [Alternative operator representations - cppreference.com](https://en.cppreference.com/w/cpp/language/operator_alternative.html#Alternative_tokens)
 
 /// Use `-DDEBUG` or `-DRELEASE` compiler options to set the implicit definition
 /// that will be read before preprocessing the source file.
-#ifdef DEBUG
-  build_configuration = "DEBUG";
-#elif RELEASE
-  build_configuration = "RELEASE";
-#else
-  build_configuration = "UNDEFINED";
-#endif
+// #ifdef DEBUG
+//   build_configuration = "DEBUG";
+// #elif RELEASE
+//   build_configuration = "RELEASE";
+// #else
+//   build_configuration = "UNDEFINED";
+// #endif
 
-  std::println("build_configuration = {0}", build_configuration);
+//  std::println("build_configuration = {0}", build_configuration);
 
-  auto result { static_cast<int>(Status::SUCCESS) };
+//  auto result { static_cast<int>(Status::SUCCESS) };
 
-  return result;
+//  return result;
+
+  return SUCCESS;
 }
 
