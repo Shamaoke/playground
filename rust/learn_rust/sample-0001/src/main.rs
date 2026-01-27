8615ea6f8c50f69c5e20393a05b1b3fabacac33d
Shamaoke <shamaoke@yandex.ru>
2026-01-27 16:08:46 +0300

    [rust] Learn Rust | Develop Sample 0001

--- rust/learn_rust/sample-0001/src/main.rs
+++ rust/learn_rust/sample-0001/src/main.rs
@@ -1,18 +1,41 @@
 
 use std::io::{stdin, Result};
+use std::cmp::Ordering::{Less, Equal, Greater};
+// or `use Ordering::*` in code
+
+use rand::Rng;
+use colored::Colorize;
 
 fn main() -> Result<()> {
 
-  println!("Guess the number!");
+  let secret: u32 = rand::rng().random_range(1..=100);
 
-  println!("Please, input your guess.");
+  // println!("The secret is: {secret}");
 
-  let mut guess = String::new();
+  println!("{ }", "Guess the number!".green());
 
-  stdin().read_line(&mut guess).expect("Failed to read line.");
+  loop {
+    println!("Please, input your guess.");
 
-  print!("You guessed: {guess}");
+    let mut guess = String::new();
 
-  Ok(())
+    stdin().read_line(&mut guess).expect("Failed to read line.");
+
+    let guess: u32 = match guess.trim().parse() {
+      Ok(n) => n,
+      Err(_) => continue,
+    };
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret) {
+      Less => println!("{ }", "Too small".red()),
+      Greater => println!("{ }", "Too big!".red()),
+      Equal => {
+        println!("{ }", "You win!".yellow());
+        break Ok(())
+      },
+    }
+  }
 }
 
