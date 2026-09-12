e7ec46b272ee15a08fa0f3a3b7e16ca78d400b5f
Shamaoke <shamaoke@yandex.ru>
2026-09-12 20:33:17 +0300

    [qt] Learn Qt | Sample 0005 | `main.cc`: use `loadFromModule` instead of manual `QUrl` and `load`; drop now-unneeded `RESOURCE_PREFIX` from `CMakeLists.txt`

--- qt/learn_qt/sample-0005/source/main.cc
+++ qt/learn_qt/sample-0005/source/main.cc
@@ -6,9 +6,12 @@ auto main(int argc, char** argv) -> int {
 
   auto app { QGuiApplication { argc, argv } };
   auto engine { QQmlApplicationEngine { } };
-  auto window { QUrl { "qrc:/learn_qt/sample_0005/views/Application.qml" } };
 
-  engine.load(window);
+  // auto window { QUrl { "qrc:/learn_qt/sample_0005/views/Application.qml" } };
+
+  // engine.load(window);
+
+  engine.loadFromModule("learn_qt.sample_0005", "Application");
 
   auto result { app.exec() };
 
